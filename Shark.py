cf3406257c2da387bffa7051634254cd3d0b5dc4
nwen24 <153663662+nwen24@users.noreply.github.com>
2023-12-12 10:32:46 -0800

    Add files via upload

--- Shark.py
+++ Shark.py
@@ -9,19 +9,20 @@ class Shark:
         self.ypos = ypos
         self.closefishx = -1
         self.closefishy = -1
-        self.image = "shark-facing-north-clear.png"
-    
-    def get_x_pos(self):
-        return self.xpos
-
-    def get_y_pos(self):
-        return self.ypos
+        
 
     def setsharkpos(self, xpos, ypos):
+
         self.xpos = xpos
         self.ypos = ypos
 
+    def getsharkpos(self):
+
+        return (self.xpos, self.ypos)
+        
+
     def move(self, closefishx, closefishy):
+
         if closefishx - self.xpos == 1 and closefishy - self.ypos == 1:
             self.xpos += 1
             self.ypos += 1
@@ -96,39 +97,69 @@ class Shark:
         
     def closestfish(self, fish1x, fish1y, fish2x, fish2y, fish3x, fish3y):
 
-        helperformula1 = (fish1x - self.xpos)**2 + (fish1y - self.ypos)**2
+        """helperformula1 = (fish1x - self.xpos)**2 + (fish1y - self.ypos)**2
         helperformula2 = (fish2x - self.xpos)**2 + (fish2y - self.ypos)**2
-        helperformula3 = (fish3x - self.xpos)**2 + (fish3y - self.ypos)**2
+        helperformula3 = (fish3x - self.xpos)**2 + (fish3y - self.ypos)**2"""
         
-        distfish1 = sqrt(float(helperformula1))
-        distfish2 = sqrt(float(helperformula2))
-        distfish3 = sqrt(float(helperformula3))
+        distfish1 = sqrt(float(((fish1x - self.xpos)**2) + ((fish1y - self.ypos)**2)))
+        distfish2 = sqrt(float(((fish2x - self.xpos)**2) + ((fish2y - self.ypos)**2)))
+        distfish3 = sqrt(float(((fish3x - self.xpos)**2) + ((fish3y - self.ypos)**2)))
 
-        xcoordslist = [distfish1, distfish2]
-        ycoordslist = []
+        randomfish = []
 
         if distfish1 < distfish2 and distfish1 < distfish3:
-            self.closestfishx = fish1x
-            self.closestfishy = fish1y
+            closestfish = (fish1x, fish1y)
         
         elif distfish2 < distfish1 and distfish2 < distfish3:
-            self.closestfishx = fish2x
-            self.closestfishy = fish2y
+            closestfish = (fish2x, fish2y)
             
         elif distfish3 < distfish1 and distfish3 < distfish2:
-            self.closestfishx = fish3x
-            self.closestfishy = fish3y
+            closestfish = (fish3x, fish3y)
 
-        #elif distfish1 == distfish2 and distfish1 < distfish3:
-         #   xcoordslist.append(fish1x)
-          #  xcoordslist.append(fish2x)
-           # ycoordslist.append(fish1y)
-            #ycoordslist.append(fish2y)
+        elif distfish1 == distfish2 and distfish1 < distfish3:
+
+            closestfish1 = (fish1x, fish1y)
+            closestfish2 = (fish2x, fish2y)
+
+            randomfish.append(closestfish1)
+            randomfish.append(closestfish2)
                                
-            #closestfishx = choice(xcoordslist)
-            #closestfishy = choice(ycoordslist)
+            closestfish = choice(randomfish)
 
-        return (self.closestfishx, self.closestfishy)
+        elif distfish1 == distfish3 and distfish1 < distfish2:
+
+            closestfish1 = (fish1x, fish1y)
+            closestfish2 = (fish3x, fish3y)
+
+            randomfish.append(closestfish1)
+            randomfish.append(closestfish2)
+                               
+            closestfish = choice(randomfish)
+
+        elif distfish2 == distfish3 and distfish2 < distfish1:
+
+            closestfish1 = (fish2x, fish2y)
+            closestfish2 = (fish3x, fish3y)
+
+            randomfish.append(closestfish1)
+            randomfish.append(closestfish2)
+                               
+            closestfish = choice(randomfish)
+
+        elif distfish1 == distfish2 == distfish3:
+
+            closestfish1 = (fish1x, fish1y)
+            closestfish2 = (fish2x, fish2y)
+            closestfish3 = (fish3x, fish3y)
+
+            randomfish.append(closestfish1)
+            randomfish.append(closestfish2)
+            randomfish.append(closestfish3)
+                               
+            closestfish = choice(randomfish)
+
+        self.closefish = closestfish
+        return (closestfish, distfish1, distfish2, distfish3, statement)
         
 
     def sharkeat(self, fishx, fishy):
